679c6d7019e8527eb3cb84976087940e43af42ed
git-ed-hub <eduardo.annk@gmail.com>
2023-09-16 16:47:37 -0600

    first commit

---
+++ src/main/java/project_web/SaludoBean.java
@@ -0,0 +1,16 @@
+package project_web;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class SaludoBean {
+	private String mensaje="Bienvenido!!!";
+	
+	public String getMensaje() {
+		return mensaje;
+	}
+	
+	public void setMensaje(String mensaje) {
+		this.mensaje= mensaje;
+	}
+}
